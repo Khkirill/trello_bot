2593ae68b0d5bb10e275ebd5f1a1364f6aab1def
Khkirill <97744368+Khkirill@users.noreply.github.com>
2022-10-17 02:37:34 +0300

    version 1.1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // Bot TOKEN
  $token = "5655249769:AAEVXMwzInCswFg6A_4t6aIAXDGG68vXVZ4";
- //$bot = new \TelegramBot\Api\Client($token);
+ $bot = new \TelegramBot\Api\Client($token);
 
  $bot->command('start', function ($message) use ($bot) {
     $answer = 'Добро пожаловать!';
