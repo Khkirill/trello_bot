9f4de95380ea3055d4d3facdbd25f36ca915dd5b
Khkirill <97744368+Khkirill@users.noreply.github.com>
2022-10-17 01:15:52 +0300

    version 1.0

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
+// composer
+require 'vendor/autoload.php';
 
-echo "Hello World";
+//Подключаем вендор
+include('vendor/autoload.php');
+
+use Telegram\Bot\Api;
+
+//Token Bot
+$telegram = new Api('5655249769:AAEVXMwzInCswFg6A_4t6aIAXDGG68vXVZ4');
+$result = $telegram -> getWebhookUpdates();
+
+$text = $result["message"]["text"];
+$chat_id = $result["message"]["chat"]["id"];
+$name = $result["message"]["from"]["username"];
+$keyboard = [["Последние статьи"],["Картинка"],["Гифка"]];
 
 ?>
