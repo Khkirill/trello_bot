c234f7711bdede9773772141cbc9c6b02fcc2417
Khkirill <97744368+Khkirill@users.noreply.github.com>
2022-10-17 01:38:04 +0300

    version 1

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-// composer
-require 'vendor/autoload.php';
-
-$bot = new \TelegramBot\Api\BotApi('5655249769:AAEVXMwzInCswFg6A_4t6aIAXDGG68vXVZ4');
-
-$bot->sendMessage($chatId, $messageText);
+ echo "SUKA";
 
 ?>
