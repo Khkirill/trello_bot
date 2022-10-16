847ca4e3aa37203729722b048448c20646f9ddb7
Khkirill <97744368+Khkirill@users.noreply.github.com>
2022-10-17 02:26:08 +0300

    version 2

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
- echo "SUKA";
+// Bot TOKEN
+ $token = "5655249769:AAEVXMwzInCswFg6A_4t6aIAXDGG68vXVZ4";
+ $bot = new \TelegramBot\Api\Client($token);
+
+ $bot->command('start', function ($message) use ($bot) {
+    $answer = 'Добро пожаловать!';
+    $bot->sendMessage($message->getChat()->getId, $answer);
+ });
 
 ?>
