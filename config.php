60867e738870f9e24dd55fb5eeea5f509f7d4d3a
Khkirill <97744368+Khkirill@users.noreply.github.com>
2022-10-17 03:50:43 +0300

    version 1.1 nice

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+const TOKEN = '5655249769:AAEVXMwzInCswFg6A_4t6aIAXDGG68vXVZ4';
+
+const API_URL = 'https://api.telegram.org/bot';
--- index.php
+++ index.php
@@ -2,38 +2,8 @@
  
  $GET_INPUT = file_get_contents('php://input');
 
- const TOKEN = '5655249769:AAEVXMwzInCswFg6A_4t6aIAXDGG68vXVZ4';
-
- const API_URL = 'https://api.telegram.org/bot';
-
- function printAnswer($str) {
-    echo "<pre>";
-    print_r($str);
-    echo "</pre>";
- }
-
- function getTelegramApi($method, $options = null) {
-    $str_request = API_URL . TOKEN . '/' . $method;
-
-    if ($options) {
-        $str_request .= '?' . http_build_query($options);
-    }
-    $request = file_get_contents($str_request);
-
-    return json_decode($request, 1);
- }
-
- function setHook($set = 1) {
-    $url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    printAnswer(
-        getTelegramApi('setWebhook',
-            [
-                'url' => $set?$url:''
-            ]
-        )
-    );
-    exit();
- }
+ require_once 'config.php';
+ require_once 'function.php';
 
  $event = json_decode($GET_INPUT, 1);
 
