35204cc849d0fd25e73e014c255aafdf32bf4d2e
Khkirill <97744368+Khkirill@users.noreply.github.com>
2022-10-17 22:16:28 +0300

    version 3.0

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 
  require_once 'config.php';
  require_once 'function.php';
- //$event['message']['text']
+ 
 
  $event = json_decode($GET_INPUT, 1);
 
  if (mb_strtolower($event['message']['text']) == '/start') {
     $autoAnswer = 'Приветствую красивый человек ' . $event['message']['from']['first_name'];
  } else {
-    $autoAnswer = 'Не знаю такие слова ' . $board['message']['text'] . "?\nХозяин пока что не научил;)";
+    $autoAnswer = 'Не знаю такие слова ' . $event['message']['text'] . "?\nХозяин пока что не научил;)";
  }
 
  getTelegramApi('sendMessage',
@@ -20,16 +20,5 @@
         'chat_id' => $event['message']['chat']['id']
     ]
 );
-
-$client = new \Trello\Client($api_key);
-$url = $client->getAuthorizationUrl('some app name', 'https://trellobot.azurewebsites.net');
-header("Location: {$url}");
-$client->setAccessToken($returned_token);
-$member_obj = new \Trello\Model\Member($client);
-$member_obj->setId('userid');
-$orgs = $member_obj->getOrganizations();
-$board = $client->getBoard($board_id);
-
-
 
 ?>
