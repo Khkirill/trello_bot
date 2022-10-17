b84828f0aabfe450b684b70f8ab4e5d32ccd31eb
Khkirill <97744368+Khkirill@users.noreply.github.com>
2022-10-17 03:19:33 +0300

    version 2

--- index.php
+++ index.php
@@ -1,13 +1,52 @@
 <?php
-// Bot TOKEN
- $token = "5655249769:AAEVXMwzInCswFg6A_4t6aIAXDGG68vXVZ4";
- $bot = new \TelegramBot\Api\Client($token);
+ 
+ $GET_INPUT = file_get_contents('php://input');
 
- $bot->command('start', function ($message) use ($bot) {
-    $answer = 'Добро пожаловать!';
-    $bot->sendMessage($message->getChat()->getId, $answer);
- });
+ const TOKEN = '5655249769:AAEVXMwzInCswFg6A_4t6aIAXDGG68vXVZ4';
 
- $bot->run();
+ const API_URL = 'https://api.telegram.org/bot';
 
+ function printAnswer($str) {
+    echo "<pre>";
+    print_r($str);
+    echo "</pre>";
+ }
+
+ function getTelegramApi($method, $options = null) {
+    $str_request = API_URL . TOKEN . '/' . $method;
+
+    if ($options) {
+        $str_request .= '?' . http_build_query($options);
+    }
+    $request = file_get_contents($str_request);
+
+    return json_decode($request, 1);
+ }
+
+ function setHook($set = 1) {
+    $url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    printAnswer(
+        getTelegramApi('setWebhook',
+            [
+                'url' => $set?$url:''
+            ]
+        )
+    );
+    exit();
+ }
+
+ $event = json_decode($GET_INPUT, 1);
+
+ if (mb_strtolower($event['message']['text']) == 'привет') {
+    $autoAnswer = 'Приветствую, тебя человек';
+ } else {
+    $autoAnswer = 'Я не знаю что это значит ' . $event['message']['text'] . "?\nМальчик не научил;)";
+ }
+
+ getTelegramApi('sendMessage',
+    [
+        'text' => $autoAnswer,
+        'chat_id' => $event['message']['chat']['id']
+    ]
+);
 ?>
