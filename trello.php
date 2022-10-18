f46594d7badd53f413842e70045ac7e55e89d411
Khkirill <97744368+Khkirill@users.noreply.github.com>
2022-10-18 05:28:45 +0300

    Update trello.php

--- trello.php
+++ trello.php
@@ -12,32 +12,32 @@ $trello = -1001377394217;
 
 $action_id = $event['action']['id'];
 
-$link = 'https://api.trello.com/1/actions/'.$action_id.'?key=dc2351312f84a4ec3fefea1147940f5e&token=e1a0db5d124db5c1758c2e93626506e73fada3a90863ebc2dc370a2dcd52641a';
+//$link = 'https://api.trello.com/1/actions/'.$action_id.'?key=dc2351312f84a4ec3fefea1147940f5e&token=e1a0db5d124db5c1758c2e93626506e73fada3a90863ebc2dc370a2dcd52641a';
 
-$ch = curl_init($link);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-$GET_INPUT_2 = curl_exec($ch);
-curl_close($ch);
+//$ch = curl_init($link);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($ch, CURLOPT_HEADER, 0);
+//$GET_INPUT_2 = curl_exec($ch);
+//curl_close($ch);
 
-$action = json_decode($GET_INPUT_2, 1);
+//$action = json_decode($GET_INPUT_2, 1);
 
 
 
-if ($action['type'] == 'moveCardFromBoard') {
-    $autoAnswer = 'Карточка была убрана с колонки ' . $action['data']['list']['name'];
- } 
+//if ($action['type'] == 'moveCardFromBoard') {
+  //  $autoAnswer = 'Карточка была убрана с колонки ' . $action['data']['list']['name'];
+ //} 
 
- if ($action['type'] == 'moveCardToBoard') {
-    $autoAnswer = 'Карточка была добавлена в колонку ' . $action['data']['list']['name'];
- }  
+ //if ($action['type'] == 'moveCardToBoard') {
+   // $autoAnswer = 'Карточка была добавлена в колонку ' . $action['data']['list']['name'];
+ //}  
 
 
 
 
 getTelegramApi('sendMessage',
    [
-       'text' => $action,
+       'text' => $action_id,
        'chat_id' => $trello
    ]
 );
