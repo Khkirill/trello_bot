b0efc52d0e78c2a0f058d4c0f170c7b33964481b
Khkirill <97744368+Khkirill@users.noreply.github.com>
2022-10-18 05:24:34 +0300

    Update trello.php

--- trello.php
+++ trello.php
@@ -12,7 +12,13 @@ $trello = -1001377394217;
 
 $action_id = $event['action']['id'];
 
-$GET_INPUT_2 = file_get_contents('https://api.trello.com/1/actions/'.$action_id.'?key=dc2351312f84a4ec3fefea1147940f5e&token=e1a0db5d124db5c1758c2e93626506e73fada3a90863ebc2dc370a2dcd52641a');
+$link = 'https://api.trello.com/1/actions/'.$action_id.'?key=dc2351312f84a4ec3fefea1147940f5e&token=e1a0db5d124db5c1758c2e93626506e73fada3a90863ebc2dc370a2dcd52641a';
+
+$ch = curl_init($link);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+$GET_INPUT_2 = curl_exec($ch);
+curl_close($ch);
 
 $action = json_decode($GET_INPUT_2, 1);
 
