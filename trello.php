a277281ce1d89ee539e65336a9b09169a56907b7
Khkirill <97744368+Khkirill@users.noreply.github.com>
2022-10-18 03:05:37 +0300

    version 4.0

--- trello.php
+++ trello.php
@@ -1,5 +1,29 @@
 <?php
 
-echo "HI GITLER";
+$GET_INPUT = file_get_contents('php://input');
+
+require_once 'config.php';
+require_once 'function.php';
+
+
+$event = json_decode($GET_INPUT, 1);
+
+if ($event['type'] == 'moveCardFromBoard') {
+   $autoAnswer = 'Карточка была убрана ';
+} 
+
+if  ($event['type'] == 'moveCardToBoard') {
+    $autoAnswer = 'Карточка была добавлена ';
+ }
+
+$trello = 1001377394217;
+
+getTelegramApi('sendMessage',
+   [
+       'text' => $autoAnswer,
+       'chat_id' => $trello
+   ]
+);
+
 
 ?>
