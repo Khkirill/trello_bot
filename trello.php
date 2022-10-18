f73641da90ca87c69c39073bca483b1f75ea17ac
Khkirill <97744368+Khkirill@users.noreply.github.com>
2022-10-18 05:05:10 +0300

    version 5.0

--- trello.php
+++ trello.php
@@ -8,23 +8,28 @@ require_once 'function.php';
 
 $event = json_decode($GET_INPUT, 1);
 
-if ($event['action']['type'] == 'moveCardFromBoard') {
-   $autoAnswer = 'Карточка была убрана ';
-} 
-
-
-
-if  ($event['action']['type'] == 'moveCardToBoard') {
-    $autoAnswer = 'Карточка была добавлена ';
- }
-
- $autoAnswer = "ZIG HAI";
-
 $trello = -1001377394217;
+
+$action_id = $event['action']['id'];
+
+$GET_INPUT_2 = file_get_contents('https://api.trello.com/1/actions/'.$action_id.'?key=dc2351312f84a4ec3fefea1147940f5e&token=e1a0db5d124db5c1758c2e93626506e73fada3a90863ebc2dc370a2dcd52641a');
+
+$action = json_decode($GET_INPUT_2, 1);
+
+if ($action['type'] == 'moveCardFromBoard') {
+    $autoAnswer = 'Карточка была убрана с колонки ' . $action['data']['list']['name'];
+ } 
+
+ if ($action['type'] == 'moveCardToBoard') {
+    $autoAnswer = 'Карточка была добавлена в колонку ' . $action['data']['list']['name'];
+ }  
+
+
+
 
 getTelegramApi('sendMessage',
    [
-       'text' => $response,
+       'text' => $autoAnswer,
        'chat_id' => $trello
    ]
 );
