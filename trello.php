996a587af98a13f9bd309779338a0edfcb086091
Khkirill <97744368+Khkirill@users.noreply.github.com>
2022-10-18 04:48:15 +0300

    Update trello.php

--- trello.php
+++ trello.php
@@ -18,21 +18,9 @@ if  ($event['action']['type'] == 'moveCardToBoard') {
     $autoAnswer = 'Карточка была добавлена ';
  }
 
- //$autoAnswer = "ZIG HAI";
+ $autoAnswer = "ZIG HAI";
 
 $trello = -1001377394217;
-
-$query = array(
-    'key' => 'dc2351312f84a4ec3fefea1147940f5e',
-    'token' => 'e1a0db5d124db5c1758c2e93626506e73fada3a90863ebc2dc370a2dcd52641a'
-  );
-  
-  $response = Unirest\Request::put(
-    'https://api.trello.com/1/boards/634d39b7f0d727005ad7c7b4',
-    $query
-  );
-
-var_dump($response);
 
 getTelegramApi('sendMessage',
    [
